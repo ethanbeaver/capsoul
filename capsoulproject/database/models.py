34ba1cae42b322506e066c00a1a42653753a8bbf
Eric Marcondes <ericmarcondes@erics-mbp.aruba.skynet.local>
2017-10-19 16:16:39 -0700

    File structure and database setup

---
+++ capsoulproject/database/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length = 30, primary_key = True)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    birthdate = models.DateField()
+    photo = models.FileField()
+    email = models.EmailField()
+    phone = models.IntegerField() 
+    location = models.TextField()
+
+    def __str__(self):
+        return self.user_name
+
+
+class Capsule(models.Model):
+    cid = models.AutoField(primary_key = True)
+    unlocks_at = models.DateTimeField()
+    owner = models.CharField(max_length = 30)
+    contributors = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
+    recipients = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
+    title = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
