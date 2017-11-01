8bd6fdddfd030b76bdbf00f69f41334853072d6f
Ethan Beaver <ethan.beaver@ahss.org>
2017-10-31 17:43:41 -0700

    Adding new server address, fixing permission issue

--- docker-volume-create.sh
+++ docker-volume-create.sh
@@ -1,5 +1,5 @@
 read -p "Enter CS Lab username:" username
-scp $username@10.9.6.102:/tmp/capsoul/capsoul-db.tar /tmp/capsoul/
+sudo scp $username@10.9.6.123:/tmp/capsoul/capsoul-db.tar /tmp/capsoul/
 docker volume create --name capsoul-db
 docker create -v /code/db --name capsoul-db-restore busybox true
 docker run --rm -v /tmp/capsoul:/backup --mount source=capsoul-db,target=/code/db busybox tar -xvf /backup/capsoul-db.tar
