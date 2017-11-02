fb46f1bd7bfa7dee189f4df08c4da48bb9009441
Ethan Beaver <ethan.beaver@ahss.org>
2017-11-02 15:24:27 -0700

    Cleaning up creation script

--- docker-volume-create.sh
+++ docker-volume-create.sh
@@ -1,6 +1,5 @@
-read -p "Enter CS Lab username:" username
-sudo wget http://docker.cs.wallawalla.edu/public/capsoul-db.tar -P /tmp/capsoul/
+wget http://docker.cs.wallawalla.edu/public/capsoul-db.tar -P ~/backups/capsoul/
 docker volume create --name capsoul-db
 docker create -v /code/db --name capsoul-db-restore busybox true
-docker run --rm -v /tmp/capsoul:/backup --mount source=capsoul-db,target=/code/db busybox tar -xvf /backup/capsoul-db.tar
+docker run --rm -v ~/backups/capsoul:/backup --mount source=capsoul-db,target=/code/db busybox tar -xvf /backup/capsoul-db.tar
 docker rm capsoul-db-restore
