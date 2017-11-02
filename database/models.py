47063b72d0d251e4249010529aea1ae97dd109ee
Eric_Marcondes <eric.marcondes@wallawalla.edu>
2017-11-02 12:15:52 -0700

    added media folder and path for upload

--- database/models.py
+++ database/models.py
@@ -10,10 +10,10 @@ class User(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     birthdate = models.DateField()
-    photo = models.FileField()
+    photo = models.ImageField(upload_to='profile_pic', blank=True)
     email = models.EmailField()
     phone = models.IntegerField() 
-    location = models.TextField()
+    location = models.TextField(default = '')
 
     def __str__(self):
         return self.user_name
@@ -25,8 +25,11 @@ class Capsule(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
     contributors = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
     recipients = models.ForeignKey(User, on_delete=models.CASCADE,related_name='+')
-    title = models.TextField()
-    description = models.TextField()
+    title = models.CharField(max_length=250)
+    description = models.TextField(default = '')
+    media = models.FileField(upload_to='media', blank=True)
+    letters = models.FileField(upload_to='letters', blank=True)
+    comments = models.TextField(default = '')
 
     def __str__(self):
         return self.title
