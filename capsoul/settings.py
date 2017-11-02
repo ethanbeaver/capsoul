26d63cfe920a9e89eb8240f5c9394eaf7d8988d8
Ethan Beaver <ethan.beaver@ahss.org>
2017-11-02 16:24:43 -0700

    Installing CORS app, allowing cross-origin requests

--- capsoul/settings.py
+++ capsoul/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'capsules.apps.CapsulesConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'capsoul.urls'
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CORS Headers fix
+CORS_ORIGIN_ALLOW_ALL = True
