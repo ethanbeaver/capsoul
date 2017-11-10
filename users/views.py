c6d957bde52de52e6e2009efe8c8a9c8c33aa512
Ethan Beaver <ethan.beaver@ahss.org>
2017-11-09 16:40:48 -0800

    Fully implementing ability to update current user

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
 
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
@@ -8,22 +9,20 @@ from django.views.decorators.http import require_http_methods, require_GET
 
 from database.models import User
 
-
 @require_http_methods(["GET", "POST"])
 def all_users(request):
     if request.method == 'GET':
-        all_users = User.objects.all().values('user_name', 'first_name', 'last_name')
+        all_users = User.objects.all().values('username', 'first_name', 'last_name')
         return JsonResponse({'users': list(all_users)}, status=200)
     else:
-        updated_fields = {'user_name': request.user.user_name}
-        for field in request.POST:
-            updated_fields[field] = request.POST.get(field)
-        current_user = User(**updated_fields)
+        fields = json.loads(request.body)
+        fields['username'] = request.user.user_name
+        current_user = User(**fields)
         current_user.save()
         return JsonResponse({"status": "resource created"}, status=200)
 
 
 @require_GET
 def specific_user(request, uname):
-    user = User.objects.filter(user_name=uname).values()
+    user = User.objects.filter(username=uname).values()
     return JsonResponse({'users': list(user)}, status=200)
