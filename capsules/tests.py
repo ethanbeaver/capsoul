a9e414671c6afb5425c441f1233fd55185dce496
Ethan Beaver <ethanbeaver35@gmail.com>
2017-11-14 01:05:04 -0800

    Fixing test

--- capsules/tests.py
+++ capsules/tests.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
-
-from views import get_letters
+from django.test import TestCase, Client
 
 # Create your tests here.
 class CapsuleViewsTestCase(TestCase):
 	def testStaticMethod(self):
-		self.assertEqual(get_letters(1,2,3),{"text": "Hey, I made this capsule for you! Hope you like it", "title": "Best Wishes",
-                         "owner": "rabery"})
+		client = Client()
+		response = client.get('/capsules/1/letters/2').content
+		self.assertEqual(response, '{"owner": "rabery", "text": "Hey, I made this capsule for you! Hope you like it", "title": "Best Wishes"}')
 		
