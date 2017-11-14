62afbf0f921566de375766b2488a6286171e7974
Ethan Beaver <ethanbeaver35@gmail.com>
2017-11-14 09:42:09 -0800

    Removing test to see code coverage before I added it

--- capsules/tests.py
+++ capsules/tests.py
@@ -5,10 +5,5 @@ import json
 from django.test import TestCase, Client
 
 # Create your tests here.
-class CapsuleViewsTestCase(TestCase):
-	def testStaticMethod(self):
-		client = Client()
-		response = json.loads(client.get('/capsules/1/letters/2').content.decode('utf8'))
-		self.assertEqual(response, {"text": "Hey, I made this capsule for you! Hope you like it", "title": "Best Wishes",
-                         "owner": "rabery"})
+
 		
