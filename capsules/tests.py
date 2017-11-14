e6a8896baf96a7e6b1558e7f510bdc8e19d1bb03
Ethan Beaver <ethanbeaver35@gmail.com>
2017-11-14 00:50:16 -0800

    Adding a basic test

--- capsules/tests.py
+++ capsules/tests.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from views import get_letters
+
 # Create your tests here.
+class CapsuleViewsTestCase(TestCase):
+	def testStaticMethod(self):
+		self.assertEqual(get_letters(1,2,3),{"text": "Hey, I made this capsule for you! Hope you like it", "title": "Best Wishes",
+                         "owner": "rabery"})
+		
